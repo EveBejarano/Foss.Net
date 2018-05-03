032741ff85097f4f80891647f93f9d02c6d2e0a8
lucilachaparro <lucilachaparro@gmail.com>
2018-05-03 20:31:55 -0300

    Add files via upload

---
+++ Hoteles/Hoteles/Models/HotelDetailsDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hoteles.Models
+{
+    public class HotelDetailsDTO
+
+    {
+        public int IDHotel { get; set; }
+        public string nombre_hotel { get; set; }
+        public string direccion_hotel { get; set; }
+        public string email_hotel { get; set; }
+        public string sitioweb_hotel { get; set; }
+        public string detalles_hotel { get; set; }
+        public string ciudad_hotel { get; set; }
+        public string nombre_pais { get; set; }
+        public byte[] Imagen_estrella { get; set; }
+        public string nombre_cadena { get; set; }
+
+    }
+}
