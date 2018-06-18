c0227143115bf225462d3acbfd965b29c5ab4181
Lucila Chaparro <lucilachaparro@gmail.com>
2018-06-18 12:50:11 -0300

    Added files

---
+++ FunTour/Hotels/Hotels/Models/City.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Hotels.Models
+{
+    public class City
+    {
+        [Key]
+        public int CityID { get; set; }
+
+        [Required]
+        public string CityName { get; set; }
+
+        [Required]
+        public string RegionID { get; set; }
+
+
+        public Region Region { get; set; }
+        public ICollection<Hotel> Hotels { get; set; }
+    }
+}
---
+++ FunTour/Hotels/Hotels/Models/Region.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Hotels.Models
+{
+    public class Region
+    {
+        [Key]
+        public int RegionID { get; set; }
+
+        [Required]
+        public string RegionName { get; set; }
+
+        [Required]
+        public string CountryID { get; set; }
+
+        public Country Country { get; set; }
+
+        public ICollection<City> Cities { get; set; }
+    }
+}
