79c990b7a35f934b4b4ab46eda0f8aa426cabfd0
lucilachaparro <lucilachaparro@gmail.com>
2018-06-18 02:22:04 -0300

    Add files via upload

---
+++ FunTour/Hotels/Hotels/Models/RoomCount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hotels.Models
+{
+    public class RoomCount
+    {
+            public int HotelID { get; set; }
+            public string HotelName { get; set; }
+            public string HotelAddress { get; set; }
+            public string HotelCity { get; set; }
+            public string HotelRegion { get; set; }
+            public string HotelCountry { get; set; }
+            public decimal StandardRate { get; set; }
+            public int FreeRoomCount { get; set; }
+    }
+
+}
