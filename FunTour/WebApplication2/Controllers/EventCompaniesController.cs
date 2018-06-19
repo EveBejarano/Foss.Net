2469f88cb6e4a906683001f688428283eb0b1745
juliandiazok <juliandiazok@hotmail.com>
2018-06-19 20:28:49 -0300

    Events Actualizado

---
+++ FunTour/WebApplication2/Controllers/EventCompaniesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Events.DAL;
+using Events.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class EventCompaniesController : Controller
+    {
+        private EventsContext db = new EventsContext();
+
+        // GET: EventCompanies
+        public ActionResult Index()
+        {
+            return View(db.EventCompanies.ToList());
+        }
+
+        // GET: EventCompanies/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EventCompany eventCompany = db.EventCompanies.Find(id);
+            if (eventCompany == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eventCompany);
+        }
+
+        // GET: EventCompanies/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EventCompanies/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CompanyID,Name,FoundedYear,Description")] EventCompany eventCompany)
+        {
+            if (ModelState.IsValid)
+            {
+                db.EventCompanies.Add(eventCompany);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(eventCompany);
+        }
+
+        // GET: EventCompanies/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EventCompany eventCompany = db.EventCompanies.Find(id);
+            if (eventCompany == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eventCompany);
+        }
+
+        // POST: EventCompanies/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CompanyID,Name,FoundedYear,Description")] EventCompany eventCompany)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(eventCompany).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(eventCompany);
+        }
+
+        // GET: EventCompanies/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EventCompany eventCompany = db.EventCompanies.Find(id);
+            if (eventCompany == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eventCompany);
+        }
+
+        // POST: EventCompanies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            EventCompany eventCompany = db.EventCompanies.Find(id);
+            db.EventCompanies.Remove(eventCompany);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ FunTour/WebApplication2/Models/EventWithTicketDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Events.Models
+{
+    public class EventWithTicketDTO
+    {
+        public int EventWithTicketID { get; set; }
+        public string Description { get; set; }
+        public string City { get; set; }
+        public string Transport { get; set; }
+        public DateTime EventDate { get; set; }
+        public string Adress { get; set; }
+        public bool HasTickets { get; set; }
+        public int MaxTickets { get; set; }
+    }
+}
