55692fc42372bf99525bb0e81759c71469d9fab6
Lucila Chaparro <lucilachaparro@gmail.com>
2018-07-05 14:20:44 -0300

    Events Fixed

---
+++ FunTour/WebApplication2/Controllers/TicketController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Events.DAL;
+using Events.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class TicketController : Controller
+    {
+        private EventsContext db = new EventsContext();
+
+        // GET: Ticket
+        public async Task<ActionResult> Index()
+        {
+            var tickets = db.Tickets.Include(t => t.Person).Include(t => t.Tick);
+            return View(await tickets.ToListAsync());
+        }
+
+        // GET: Ticket/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ticket ticket = await db.Tickets.FindAsync(id);
+            if (ticket == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ticket);
+        }
+
+        // GET: Ticket/Create
+        public ActionResult Create()
+        {
+            ViewBag.PersonID = new SelectList(db.Persons, "PersonID", "DNI");
+            ViewBag.TicketID = new SelectList(db.Ticks, "TicketID", "TicketID");
+            return View();
+        }
+
+        // POST: Ticket/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ReservaID,TicketID,PersonID")] Ticket ticket)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tickets.Add(ticket);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.PersonID = new SelectList(db.Persons, "PersonID", "DNI", ticket.PersonID);
+            ViewBag.TicketID = new SelectList(db.Ticks, "TicketID", "TicketID", ticket.TicketID);
+            return View(ticket);
+        }
+
+        // GET: Ticket/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ticket ticket = await db.Tickets.FindAsync(id);
+            if (ticket == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PersonID = new SelectList(db.Persons, "PersonID", "DNI", ticket.PersonID);
+            ViewBag.TicketID = new SelectList(db.Ticks, "TicketID", "TicketID", ticket.TicketID);
+            return View(ticket);
+        }
+
+        // POST: Ticket/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ReservaID,TicketID,PersonID")] Ticket ticket)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ticket).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.PersonID = new SelectList(db.Persons, "PersonID", "DNI", ticket.PersonID);
+            ViewBag.TicketID = new SelectList(db.Ticks, "TicketID", "TicketID", ticket.TicketID);
+            return View(ticket);
+        }
+
+        // GET: Ticket/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ticket ticket = await db.Tickets.FindAsync(id);
+            if (ticket == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ticket);
+        }
+
+        // POST: Ticket/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Ticket ticket = await db.Tickets.FindAsync(id);
+            db.Tickets.Remove(ticket);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ FunTour/WebApplication2/Models/Tick.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Events.Models
+{
+    public class Tick //ESTO ES UN TICKET
+    {
+        [Key]
+        [Required]
+        public int TicketID { get; set; }
+
+        [ForeignKey("EventWithTicket")]
+        public int EventWithTicketID { get; set; }
+        public virtual EventWithTicket EventWithTicket { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
