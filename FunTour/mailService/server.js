76e654ffb31990a8c7f0028ff5104423e4af8046
Wenceslao Facundo Marquez Burgos <wenceslao1207@protonmail.com>
2018-07-10 04:00:41 -0300

    mailService2

--- FunTour/mailService/server.js
+++ FunTour/mailService/server.js
@@ -6,30 +6,6 @@ var Publicity = require('./api/models/publicityModel.js');
 var bodyParser = require('body-parser');
 var multer = require('multer');
 
-/*var storage = multer.diskStorage({
-	destination: function (req, file, callback) {
-		callback(null, '../FunTour/Newsletter');
-	},
-	filename: function (req, file, callback) {
-		callback(null, file.originalname);
-	}
-});
-
-var upload = multer({ storage : storage}).single('Newsletter');
-
-app.get('/', function(req, res) {
-	res.sendFile(__dirname + "/index.html");
-});
-
-app.post('/api/file', function(req, res) {
-	upload(req, res, function (err) {
-		if (err)
-			return res.send(err);
-		res.end("FileDone");
-	});
-});
-*/
-
 mongoose.connect('mongodb://localhost/publicityDB');
 
 app.use(bodyParser.urlencoded({ extended: true }));
